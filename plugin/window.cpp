349ca4099bc8239ba242892a591f6cac132d2742
Madghostek <anaruk2@tlen.pl>
2020-05-19 20:37:33 +0200

    Add files via upload
    minor changes such as turining sprintf to sprintf_s

---
+++ plugin/window.cpp
@@ -0,0 +1,106 @@
+//debug window that clogs up the screen
+#include <windows.h>
+#include <fstream>
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+#include <time.h>
+#include "window.hpp"
+#include "config.hpp"
+HWND label;
+DWORD Value;
+int id,msgCount = 0; //debug id for testing
+
+typedef struct {
+    BOOL showDebug;
+} CONFIG;
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch(uMsg)
+    {
+    break;
+    case WM_CREATE:
+    {
+        label = CreateWindow("STATIC","Plugin loaded",WS_VISIBLE|WS_CHILD,5,5,100,20,hwnd,NULL,GetModuleHandle(0),NULL);
+    }
+    break;
+    case WM_COPYDATA:
+        {
+            char txt[10];
+            sprintf_s(txt,sizeof(txt),"count: %i",++msgCount);
+            SendMessage(label,WM_SETTEXT,0,(LPARAM)txt);
+            COPYDATASTRUCT* data = (COPYDATASTRUCT*)lParam;
+            Value = *(DWORD*)(data->lpData);
+        }
+    break;
+    case WM_DESTROY:
+    {
+        PostQuitMessage(0);
+    }
+    break;
+    default:
+        {
+            return DefWindowProc(hwnd, uMsg,wParam,lParam);
+        }
+    }
+    return 0;
+}
+
+void ReadConfig(CONFIG*cfg)
+{
+    std::fstream cFile(".Receiver.cfg");
+    if (!cFile.is_open())
+    {
+        cFile.open(".Receiver.cfg", std::fstream::out);
+        cFile << "Show=0";
+        cfg->showDebug = 0;
+    }
+    else{
+        std::string show;
+        getline (cFile, show);
+        std::string val = show.substr(show.find("=")+1,1);
+        cfg->showDebug = val == "1";
+        cFile.close();
+    }
+}
+
+DWORD WindowMain(HINSTANCE h)
+{
+    CONFIG cfg;
+    ReadConfig(&cfg);
+    const char CLASS_NAME[]  = "ReceiverClass";
+    //MSG msg;
+    WNDCLASS wc = { };
+
+    wc.lpfnWndProc   = WindowProc;
+    wc.hInstance     = h;
+    wc.hCursor       = LoadCursor(NULL, IDC_ARROW);
+    wc.lpszClassName = CLASS_NAME;
+    wc.hbrBackground = (HBRUSH)(COLOR_BACKGROUND);
+
+    RegisterClass(&wc);
+
+    HWND hwnd = CreateWindowEx(
+        0,                              // Optional window styles.
+        CLASS_NAME,                     // Window class
+        "debug",    // Window text
+        WS_CHILD|WS_VISIBLE,            // Window style
+        CW_USEDEFAULT, CW_USEDEFAULT, 200,50, // Size and position
+        FindWindow("myWindowClass",0), //parent
+        NULL,       // Menu
+        h,  // Instance handle (main thread)
+        NULL        // Additional application data
+    );
+    std::srand(time(0));
+    id=std::rand()%1000;
+    ShowWindow(hwnd,cfg.showDebug ? SW_SHOW : SW_HIDE);
+    //this will be handled by mupen!
+    //while(GetMessage(&msg, NULL, 0, 0) > 0)
+    //{
+    //    TranslateMessage(&msg);
+    //    DispatchMessage(&msg);
+    //}
+    //return msg.wParam;
+    return 0;
+}
---
+++ plugin/window.hpp
@@ -0,0 +1,7 @@
+#ifndef WINDOW_H_INCLUDED
+#define WINDOW_H_INCLUDED
+
+#define ID_READY 42069
+DWORD WindowMain(HINSTANCE hInstance);
+extern DWORD Value;
+#endif // WINDOW_H_INCLUDED
